9872894106ccc0643598c1fd04eb88098a509829
Cody Lindley <contact@codylindley.com>
2015-02-11 16:28:48 -0700

    no lunch, hours of work, I can only hope someone notices

---
+++ telerik:kendo-ui-core-bootstrap-theme/package.js
@@ -0,0 +1,108 @@
+Package.describe({
+  name: "telerik:kendo-ui-core-bootstrap-theme",
+  summary: "Kendo UI Core is the free & open-source version of Kendo UI, the web's best UI widgets!",
+  version: "1119.0.11",
+  git: 'https://github.com/kendo-labs/meteor-packages.git',
+  documentation:'README.md'
+});
+
+Package.onUse(function (api) {
+  api.versionsFrom(['METEOR@0.9.0', 'METEOR@1.0']);
+
+  api.use('jquery', 'client');
+
+  //js
+
+  api.add_files('telerik.kendoui.core/src/src/kendo.ui.core.js', 'client');
+
+  //css
+
+  api.add_files('telerik.kendoui.core/src/styles/web/kendo.common.core.css', 'client');
+
+  api.add_files('telerik.kendoui.core/src/styles/web/kendo.bootstrap.css', 'client');
+
+  //mobile css
+
+  api.add_files('telerik.kendoui.core/src/styles/mobile/kendo.mobile.all.css', 'client');
+
+  //global CSS fonts
+
+  api.add_files('telerik.kendoui.core/styles/images/kendoui.ttf', 'client');
+
+  api.add_files('telerik.kendoui.core/styles/images/kendoui.woff', 'client');
+
+  api.add_files('telerik.kendoui.core/src/styles/mobile/images/kendoui.woff', 'client');
+
+  api.add_files('telerik.kendoui.core/src/styles/mobile/images/kendoui.ttf', 'client');
+
+  //global CSS images
+
+  api.add_files('telerik.kendoui.core/styles/images/back.png', 'client');
+
+  api.add_files('telerik.kendoui.core/styles/images/kendoui.svg', 'client');
+
+  api.add_files('telerik.kendoui.core/styles/images/wp8_icons.png', 'client');
+
+  api.add_files('telerik.kendoui.core/styles/images/wp8_inverseicons.png', 'client');
+
+  //global CSS textures
+
+  api.add_files('telerik.kendoui.core/src/styles/web/textures/brushed-metal.png', 'client');
+  api.add_files('telerik.kendoui.core/src/styles/web/textures/dots1.png', 'client');
+  api.add_files('telerik.kendoui.core/src/styles/web/textures/dots2.png', 'client');
+  api.add_files('telerik.kendoui.core/src/styles/web/textures/dots3.png', 'client');
+  api.add_files('telerik.kendoui.core/src/styles/web/textures/dots4.png', 'client');
+  api.add_files('telerik.kendoui.core/src/styles/web/textures/dots5.png', 'client');
+  api.add_files('telerik.kendoui.core/src/styles/web/textures/dots6.png', 'client');
+  api.add_files('telerik.kendoui.core/src/styles/web/textures/dots7.png', 'client');
+  api.add_files('telerik.kendoui.core/src/styles/web/textures/dots8.png', 'client');
+  api.add_files('telerik.kendoui.core/src/styles/web/textures/dots9.png', 'client');
+  api.add_files('telerik.kendoui.core/src/styles/web/textures/dots10.png', 'client');
+  api.add_files('telerik.kendoui.core/src/styles/web/textures/dots11.png', 'client');
+  api.add_files('telerik.kendoui.core/src/styles/web/textures/dots12.png', 'client');
+  api.add_files('telerik.kendoui.core/src/styles/web/textures/dots13.png', 'client');
+
+  api.add_files('telerik.kendoui.core/src/styles/web/textures/glass-lighter.png', 'client');
+  api.add_files('telerik.kendoui.core/src/styles/web/textures/glass.png', 'client');
+  api.add_files('telerik.kendoui.core/src/styles/web/textures/highlight.png', 'client');
+  api.add_files('telerik.kendoui.core/src/styles/web/textures/hsv-gradient.png', 'client');
+  api.add_files('telerik.kendoui.core/src/styles/web/textures/hue.png', 'client');
+  api.add_files('telerik.kendoui.core/src/styles/web/textures/leather1.png', 'client');
+  api.add_files('telerik.kendoui.core/src/styles/web/textures/leather2.png', 'client');
+  api.add_files('telerik.kendoui.core/src/styles/web/textures/noise.png', 'client');
+  api.add_files('telerik.kendoui.core/src/styles/web/textures/stripe1.png', 'client');
+  api.add_files('telerik.kendoui.core/src/styles/web/textures/stripe2.png', 'client');
+  api.add_files('telerik.kendoui.core/src/styles/web/textures/stripe3.png', 'client');
+  api.add_files('telerik.kendoui.core/src/styles/web/textures/stripe4.png', 'client');
+  api.add_files('telerik.kendoui.core/src/styles/web/textures/stripe5.png', 'client');
+  api.add_files('telerik.kendoui.core/src/styles/web/textures/stripe6.png', 'client');
+  api.add_files('telerik.kendoui.core/src/styles/web/textures/transparency.png', 'client');
+  api.add_files('telerik.kendoui.core/src/styles/web/textures/transtexture.png', 'client');
+
+  //theme
+
+  api.add_files('telerik.kendoui.core/src/styles/web/Bootstrap/editor.png', 'client');
+
+  api.add_files('telerik.kendoui.core/src/styles/web/Bootstrap/imagebrowser.png', 'client');
+
+  api.add_files('telerik.kendoui.core/src/styles/web/Bootstrap/indeterminate.gif', 'client');
+
+  api.add_files('telerik.kendoui.core/src/styles/web/Bootstrap/loading-image.gif', 'client');
+
+  api.add_files('telerik.kendoui.core/src/styles/web/Bootstrap/loading.gif', 'client');
+
+  api.add_files('telerik.kendoui.core/src/styles/web/Bootstrap/loading_2x.gif', 'client');
+
+  api.add_files('telerik.kendoui.core/src/styles/web/Bootstrap/slider-h.gif', 'client');
+
+  api.add_files('telerik.kendoui.core/src/styles/web/Bootstrap/slider-v.gif', 'client');
+
+  api.add_files('telerik.kendoui.core/src/styles/web/Bootstrap/sprite.png', 'client');
+
+  api.add_files('telerik.kendoui.core/src/styles/web/Bootstrap/sprite_2x.png', 'client');
+
+  api.add_files('telerik.kendoui.core/src/styles/web/Bootstrap/sprite_kpi.png', 'client');
+
+  api.add_files('telerik.kendoui.core/src/styles/web/Bootstrap/sprite_kpi_2x.png', 'client');
+
+});
---
+++ telerik:kendo-ui-core-flat-theme/README.md
@@ -0,0 +1,48 @@
+# Kendo UI Core Meteor packages by theme
+
+Provides a simple way to pull [Kendo UI core](https://github.com/telerik/kendo-ui-core) into Meteor by theme. Includes both Kendo UI core and specific theme.
+
+You can view all the themes here: [http://demos.telerik.com/kendo-ui/themebuilder/](http://demos.telerik.com/kendo-ui/themebuilder/)
+
+## Usage (i.e. run from your app directory using meteor CLI)
+
+```
+$ meteor add telerik:kendo-ui-core-[NAME OF THEME]-theme
+```
+
+Available themes:
+
+```
+$ meteor add telerik:kendo-ui-core-default-theme
+
+$ meteor add telerik:kendo-ui-core-black-theme
+
+$ meteor add telerik:kendo-ui-core-blueopal-theme
+
+$ meteor add telerik:kendo-ui-core-bootstrap-theme
+
+$ meteor add telerik:kendo-ui-core-silver-theme
+
+$ meteor add telerik:kendo-ui-core-uniform-theme
+
+$ meteor add telerik:kendo-ui-core-metro-theme
+
+$ meteor add telerik:kendo-ui-core-metroblack-theme
+
+$ meteor add telerik:kendo-ui-core-highcontrast-theme
+
+$ meteor add telerik:kendo-ui-core-moonlight-theme
+
+$ meteor add telerik:kendo-ui-core-flat-theme
+
+$ meteor add telerik:kendo-ui-core-material-theme
+
+$ meteor add telerik:kendo-ui-core-materialblack-theme
+
+```
+
+Notes: 
+
+* use one package, per Meteor application
+* all mobile themes are being included
+* bootstrap package is not a dependency of the Kendo UI core bootstrap theme you'll have to add it i.e. ```$ meteor add twbs:bootstrap```
